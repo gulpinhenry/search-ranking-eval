baf72cbb79fb495dacd2f14992d9b0fa654a2968
henry <henry.j.kam@gmail.com>
2024-12-06 16:39:52 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.9.2-linux-x86_64.tar.gz
-wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.9.2-linux-x86_64.tar.gz.sha512
-tar -xzf elasticsearch-oss-7.9.2-linux-x86_64.tar.gz
-sudo chown -R daemon:daemon elasticsearch-7.9.2/
-shasum -a 512 -c elasticsearch-oss-7.9.2-linux-x86_64.tar.gz.sha512
+# wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.9.2-linux-x86_64.tar.gz
+# wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.9.2-linux-x86_64.tar.gz.sha512
+# tar -xzf elasticsearch-oss-7.9.2-linux-x86_64.tar.gz
+# sudo chown -R daemon:daemon elasticsearch-7.9.2/
+# shasum -a 512 -c elasticsearch-oss-7.9.2-linux-x86_64.tar.gz.sha512
 
 mkdir -p datasets
 pip install -r requirements.txt
